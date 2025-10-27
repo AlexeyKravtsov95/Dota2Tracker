ee60875f1e5d6dd6931971b8155e7215e9a3a6fb
aleksey.kravtsov <aleksey.kravtsov@vk.team>
2025-10-27 16:33:23 +0700

    Init project

---
+++ Dota2Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dota2Tracker
+//
+//  Created by Алексей Кравцов on 27.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
